bda2b8f5b454544786d24e38df597758ccc9fff5
Jia Amamangpang <40689534+jiammngpng@users.noreply.github.com>
2021-02-10 21:35:16 +0800

    Add files via upload

---
+++ BackPropagation/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace BackPropagation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dimensionBox = new System.Windows.Forms.TextBox();
+            this.btnRescale = new System.Windows.Forms.Button();
+            this.newDimBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGrayscale = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(48, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(299, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk_1);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(527, 46);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnLoad.Size = new System.Drawing.Size(75, 28);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load Image";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(527, 80);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnSave.Size = new System.Drawing.Size(75, 31);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dimensions:";
+            // 
+            // dimensionBox
+            // 
+            this.dimensionBox.Location = new System.Drawing.Point(128, 260);
+            this.dimensionBox.Name = "dimensionBox";
+            this.dimensionBox.Size = new System.Drawing.Size(120, 20);
+            this.dimensionBox.TabIndex = 5;
+            // 
+            // btnRescale
+            // 
+            this.btnRescale.Location = new System.Drawing.Point(527, 117);
+            this.btnRescale.Name = "btnRescale";
+            this.btnRescale.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnRescale.Size = new System.Drawing.Size(75, 28);
+            this.btnRescale.TabIndex = 6;
+            this.btnRescale.Text = "Rescale";
+            this.btnRescale.UseVisualStyleBackColor = true;
+            this.btnRescale.Click += new System.EventHandler(this.btnRescale_Click);
+            // 
+            // newDimBox
+            // 
+            this.newDimBox.Location = new System.Drawing.Point(391, 263);
+            this.newDimBox.Name = "newDimBox";
+            this.newDimBox.Size = new System.Drawing.Size(108, 20);
+            this.newDimBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "New Dimensions:";
+            // 
+            // btnGrayscale
+            // 
+            this.btnGrayscale.Location = new System.Drawing.Point(527, 151);
+            this.btnGrayscale.Name = "btnGrayscale";
+            this.btnGrayscale.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnGrayscale.Size = new System.Drawing.Size(75, 28);
+            this.btnGrayscale.TabIndex = 9;
+            this.btnGrayscale.Text = "Grayscale";
+            this.btnGrayscale.UseVisualStyleBackColor = true;
+            this.btnGrayscale.Click += new System.EventHandler(this.btnGrayscale_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 341);
+            this.Controls.Add(this.btnGrayscale);
+            this.Controls.Add(this.newDimBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRescale);
+            this.Controls.Add(this.dimensionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Back Propagation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dimensionBox;
+        private System.Windows.Forms.Button btnRescale;
+        private System.Windows.Forms.TextBox newDimBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGrayscale;
+    }
+}
+
---
+++ BackPropagation/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackPropagation
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded, processed;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void btnRescale_Click(object sender, EventArgs e)
+        {
+            Scale(ref loaded, ref processed);
+            pictureBox2.Image = this.processed;
+            newDimBox.Text = Convert.ToString(this.processed.Width) + ", " + Convert.ToString(this.processed.Height);
+        }
+
+        public static void Scale(ref Bitmap a, ref Bitmap b)
+        {
+            int targetSize = 100;
+            int width = a.Width, height = a.Height, x, y, xSource, ySource;
+            float nPercentW = ((float)targetSize / (float)width);
+            float nPercentH = ((float)targetSize / (float)height);
+
+            float nPercent;
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            //new width
+            int destWidth = (int)(width * nPercent);
+            //new height
+            int destHeight = (int)(height * nPercent);
+            
+            b = new Bitmap(destWidth, destHeight);
+
+
+            for (x = 0; x < destWidth; x++)
+            {
+                for (y = 0; y < destHeight; y++)
+                {
+                    xSource = x * width / destWidth;
+                    ySource = y * height / destHeight;
+                    b.SetPixel(x, y, a.GetPixel(xSource, ySource));
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            int width, height;
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+            width = pictureBox1.Image.Width;
+            height = pictureBox1.Image.Height;
+            dimensionBox.Text = Convert.ToString(width) + "," + Convert.ToString(height);
+        }
+
+        private void btnGrayscale_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(processed.Width, processed.Height);
+            for (int x = 0; x < processed.Width; x++)
+            {
+                for (int y = 0; y < processed.Height; y++)
+                {
+                    Color pixel = processed.GetPixel(x, y);
+                    byte grey = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                    bm.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            pictureBox2.Image = bm;
+        }
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
